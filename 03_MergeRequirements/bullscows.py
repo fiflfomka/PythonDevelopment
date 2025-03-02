19d592d2279274cea68f7f3ee7b901420c6e78a9
Dmitry Fomin <d.fomin@ispras.ru>
2025-03-02 13:28:53 +0300

    [03] add program body

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,16 @@
+import os
+import sys
 import random
+import argparse
+import urllib.request
+
+
+parser = argparse.ArgumentParser(prog=os.path.basename(
+    sys.argv[0]), description="Bulls and cows game")
+parser.add_argument(type=str, dest="dict_file",
+                    help="Path or URL of dictionary")
+parser.add_argument(type=int, default=5, dest="wordlen",
+                    help="what length of words do you want to use")
 
 
 def bullscows(guess: str, answer: str):
@@ -26,5 +38,29 @@ def gameplay(ask: callable, inform: callable, words: list[str]) -> int:
         inform("Быки: {}, Коровы: {}", *bullscows(guess, answer))
         if guess == answer:
             return i
-    
-        
+
+
+def ask(prompt: str, valid: list[str] = None) -> str:
+    while True:
+        s = input(prompt)
+        if valid is not None and s not in valid:
+            print("Word", s, "not in the dictionary. Try again!")
+            continue
+        return s
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    try:
+        with open(args.dict_file, "r") as f:
+            data = f.read()
+    except Exception as e1:
+        try:
+            data = urllib.request.urlopen(args.dict_file).read()
+        except Exception as e2:
+            print("can't open dictionary:\n", e1, "\n", e2)
+            sys.exit(1)
+    all_words = (p.strip() for p in data.split("\n"))
+    good_words = list(filter(lambda x: len(x) == args.wordlen, all_words))
+    res = gameplay(ask, print, good_words)
+    print(f"You asked {res} times")
